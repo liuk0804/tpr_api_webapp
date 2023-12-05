bdaa2fe57c763dbb052b21ab14205e10b89fad73
Kun Liu <liuk0804@gmail.com>
2023-12-04 16:14:32 -0800

    mod main to setup initial fast api app

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import fastapi
+import uvicorn
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = fastapi.FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/")
+def index():
+    return {"message": "Hello, World"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+uvicorn.run(app)
