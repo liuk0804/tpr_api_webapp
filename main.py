38a69f9c2ae69f0929233b6b12c7a3a1d79e5853
Kun Liu <liuk0804@gmail.com>
2023-12-04 16:48:08 -0800

    add router view folder

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import fastapi
 import uvicorn
+import fastapi_chameleon
+from fastapi_chameleon import template
+
 
 app = fastapi.FastAPI()
+fastapi_chameleon.global_init("templates")
 
 
 @app.get("/")
-def index():
-    return {"message": "Hello, World"}
+@template(template_file="index.html")
+def index(user_name: str = "topaz"):
+    return {"user_name": user_name}
 
 
 if __name__ == "__main__":
